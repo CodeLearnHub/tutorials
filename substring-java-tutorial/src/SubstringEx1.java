816b3c2bc8e029390e998e63693d7f58c21dd2a5
geonikpal <geonikpal@gmail.com>
2021-11-20 18:33:24 +0200

    Substring Java Tutorial

---
+++ substring-java-tutorial/src/SubstringEx1.java
@@ -0,0 +1,12 @@
+public class SubstringEx1 {
+        public static void main(String args[])
+        {
+            int beginning=23;
+            String str ="CodeLearnHub substring Tutorial";
+
+
+            System.out.print("The substring  is : ");
+            System.out.println(Str.substring(beginning));
+        }
+    }
+
