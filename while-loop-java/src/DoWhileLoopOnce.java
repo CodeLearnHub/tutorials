12f47a8ec1c23513b83f8617a86aa1418e6b9199
CodeLearnHub <93278003+CodeLearnHub@users.noreply.github.com>
2021-12-30 16:22:20 +0200

    Add files via upload

---
+++ while-loop-java/src/DoWhileLoopOnce.java
@@ -0,0 +1,10 @@
+public class DoWhileLoopOnce {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            System.out.println("This code was executed only once.");
+            i++;
+        }
+        while (i < 0);
+    }
+}
