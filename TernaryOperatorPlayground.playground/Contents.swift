172057de0ff2c48510ecc4930b8baebd2c38b67a
Dimitris Tasios <dtasios4@gmail.com>
2022-03-12 22:44:46 +0200

    add limitation of ternary operators section

--- TernaryOperatorPlayground.playground/Contents.swift
+++ TernaryOperatorPlayground.playground/Contents.swift
@@ -45,3 +45,18 @@ if number >= 3 {
 } else {
     print("\(number) is not between 3 and 7.")
 }
+
+// MARK: Limitation of ternary operators
+// the following cannot be written with a ternary operator
+var aNumber = 10
+if aNumber == 10 {
+    print(aNumber)
+} else {
+    aNumber = 0
+}
+
+let invalid = (aNumber == 10 ? print(aNumber) : 0) // INVALID: Result values in '? :' expression have mismatching types '()' and 'Int'
+(aNumber == 10 ? print(aNumber) : 0)               // INVALID: same error as above
+
+aNumber == 10 ? print(aNumber) : print(0)          // VALID: it isn't saved in a variable since it doesnt return anything
+let alsoValid = (aNumber == 10 ? aNumber : 0)      // VALID: each path returns an integer, which is saved on our constant "alsoValid"
