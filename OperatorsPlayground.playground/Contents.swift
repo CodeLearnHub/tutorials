49a4ef4bf40b7b9bf63c35dbcc4461e8bb251347
Dimitris Tasios <dtasios4@gmail.com>
2022-04-17 15:49:13 +0300

    almost completed arithmetic operations

---
+++ OperatorsPlayground.playground/Contents.swift
@@ -0,0 +1,72 @@
+// MARK: Swift Operators
+
+// MARK: - Assignment Operators
+
+// MARK: Assignment Operator, =
+var myVar = 4 // the assignment operator "=" creates a new variable with the value "4"
+myVar = 5 // the assignment operator updates the variable with the value "5"
+
+let aString = "Hello world!" // assigning a String value to a constant
+
+// the following cannot be done, since they don't have the same type (Int assigned to String variable)
+var anotherString = "Hello again"
+//anotherString = 5 // compilation error
+
+// MARK: Addition Assignment Operator, +=
+let myConstant = 3
+var varForAddition = 5
+varForAddition += myConstant // 8, this is equal to varForAddition = varForAddition + myConstant
+
+var messageForConcatenation = "Code Learn"
+messageForConcatenation += " Hub" // "Code Learn Hub", concatenation between Strings, with the += operator
+
+// MARK: Subtraction Assignment Operator, -=
+var varForSubtraction = 10
+varForSubtraction -= 4 // 6, equal to varForSubtraction = varForSubtraction - 4
+
+// MARK: Multiplication Assignment Operator, *=
+var varForMultiplication = 7
+varForMultiplication *= 8 // 56, equal to varForMultiplication = varForMultiplication * 4
+
+// MARK: Division Assignment Operator, /=
+var varForDivision = 100
+varForDivision /= 25 // 4, equal to varForDivision = varForDivision / 4
+
+// MARK: Remainder Assignment Operator, %=
+var varForRemainder = 100
+varForRemainder /= 25 // 0, equal to varForDivision = varForDivision % 4
+
+// MARK: - Arithmetic Operators
+
+// MARK: Addition Operator, +
+let addition = 5 + 2 // 7
+let clh = "Code " + "Learn Hub"
+
+// MARK: Subtraction Operator, "-"
+let subtraction = 100 - 99 // 1
+
+// MARK: Multiplication Operator, *
+let multiplication = 30 * 3 // 90
+
+// MARK: Division Operator, /
+let division = 30 / 2 // 15
+
+let divisionOfInts = 30 / 4 // 7
+let divisionOfDoubles = 30 / 4.0 // 7.5, same as 30.0 / 4 and 30.0 / 4.0
+
+// MARK: Remainder or Modulo Operator, %
+let remainderOrModulo = 30.0 % 4 // 7
+// MARK: Unary Minus Operator
+// MARK: Unary Plus Operator
+
+
+// MARK: - Comparison Operators
+
+
+// MARK: - Logical Operators
+
+
+// MARK: - Bitwise Operators
+
+
+// MARK: - Miscellaneous Operators
