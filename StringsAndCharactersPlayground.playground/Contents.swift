314b5d3dfd31ab81c741796a6c1a95e75715edc1
Dimitris Tasios <dtasios4@gmail.com>
2022-05-01 13:12:32 +0300

    Characters example

--- StringsAndCharactersPlayground.playground/Contents.swift
+++ StringsAndCharactersPlayground.playground/Contents.swift
@@ -1,6 +1,11 @@
 // MARK: Strings and Characters
 
 // MARK: Characters
+let letterD: Character = "D" // type: Character
+let smirkEmoji: Character = "😏"  // also valid
+let stringD = "D" // by default, this is a String
 
+// Invalid Character
+//let invalidCharacter: Character = "Hello" // Character must only have one character
 
 // MARK: Strings
