22424cea8fd2bf24507e8dc4514c79cd4f5e2cce
Dimitris Tasios <dtasios4@gmail.com>
2022-07-04 19:10:49 +0300

    initial commit, playground structure

---
+++ SwitchStatementPlayground.playground/Contents.swift
@@ -0,0 +1,33 @@
+// MARK: Swift switch Statement
+
+// MARK: - 1. What Is a switch Statement in Swift?
+
+
+// MARK: - 2. Characteristics of Swift's switch Statements
+
+
+    // MARK: 2.1 A Switch Statement Will Only Execute a Single Case by Default
+
+
+    // MARK: 2.2 Match Multiple Values in a Range
+
+
+    // MARK: 2.3 Switching on Tuples to Check for Each of Their Elements
+
+
+    // MARK: 2.4 Using Value Bindings to use Variables or Constants Inside the Case
+
+
+    // MARK: 2.5 Using a Where Clause to Check for Additional Criteria
+
+
+    // MARK: 2.6 Combining Multiple Cases at Once With Compound Cases
+
+
+// MARK: - 3. Comparing With if Statements
+
+
+    // MARK: 3.1 Using an if Statement
+
+
+    // MARK: 3.2 Using a switch Statement
