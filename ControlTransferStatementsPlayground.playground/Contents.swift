7bcb0b8678b3348a41120502a3fecdc93bde8d1a
Dimitris Tasios <dtasios4@gmail.com>
2022-07-12 21:39:55 +0300

    add helper file for marking headings

--- ControlTransferStatementsPlayground.playground/Contents.swift
+++ ControlTransferStatementsPlayground.playground/Contents.swift
@@ -1,27 +1,24 @@
 // MARK: Swift Control Transfer Statements
 
-private func markHeading3(subsectionNumber: String) {
-    print("\n------------Subection \(subsectionNumber)------------\n")
-}
 // MARK: - 2. The Control Transfer Statements
 
     // MARK: 2.1 The break Keyword
-    markHeading3(subsectionNumber: "2.1")
+    markHeading3(title: "\(HeadingWordings.section) 2.1")
 
     // MARK: 2.2 The continue Keyword
-    markHeading3(subsectionNumber: "2.2")
+    markHeading3(title: "\(HeadingWordings.section) 2.2")
 
     // MARK: 2.3 The return Keyword
-    markHeading3(subsectionNumber: "2.3")
+    markHeading3(title: "\(HeadingWordings.section) 2.3")
 
     // MARK: 2.4 The fallthrough Keyword
-    markHeading3(subsectionNumber: "2.4")
+    markHeading3(title: "\(HeadingWordings.section) 2.4")
 
     // MARK: 2.5 The throws Keyword
-    markHeading3(subsectionNumber: "2.5")
+    markHeading3(title: "\(HeadingWordings.section) 2.5")
 
     // MARK: 2.6 The guard Keyword
-    markHeading3(subsectionNumber: "2.6")
+    markHeading3(title: "\(HeadingWordings.section) 2.6")
 
     // MARK: 2.7 Labeled Statements
-    markHeading3(subsectionNumber: "2.7")
+    markHeading3(title: "\(HeadingWordings.section) 2.7")
