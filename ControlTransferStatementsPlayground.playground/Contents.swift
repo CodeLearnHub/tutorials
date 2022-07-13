cdd997de5e718cdc2b5e60dc9d70f1286c6d5c55
Dimitris Tasios <dtasios4@gmail.com>
2022-07-13 22:00:40 +0300

    finish break keyword

--- ControlTransferStatementsPlayground.playground/Contents.swift
+++ ControlTransferStatementsPlayground.playground/Contents.swift
@@ -1,5 +1,7 @@
 // MARK: Swift Control Transfer Statements
 
+import Foundation
+
 // MARK: - 2. The Control Transfer Statements
 
     // MARK: 2.1 The break Keyword
@@ -29,6 +31,32 @@
 
         // MARK: 2.1.2 Using break inside a switch statement
         markHeading4(title: "\(HeadingWordings.section) 2.1.2")
+
+        private func switchBreakExample(number: Int) {
+            switch number {
+            case 1, 2, 3, 4, 5:
+                print("case for 1-5")
+            case 6:
+                break // don't handle this case
+            case 7, 8, 9:
+                let message = "Your number is: "
+                if number == 7 {
+                    print("\(message)a Lucky seven")
+                    break
+                }
+                print("8 or 9")
+            default:
+                print("Your number is \(number)")
+            }
+            
+            print("Thank you\n")
+        }
+
+        switchBreakExample(number: 2)
+        switchBreakExample(number: 6)
+        switchBreakExample(number: 7)
+        switchBreakExample(number: 9)
+        switchBreakExample(number: 110)
 
     // MARK: 2.2 The continue Keyword
     markHeading3(title: "\(HeadingWordings.section) 2.2")
