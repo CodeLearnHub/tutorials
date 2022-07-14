04c4fe90f1bcc9d4e7c15f07b29f1f7488646b1b
Dimitris Tasios <dtasios4@gmail.com>
2022-07-14 21:08:28 +0300

    more sections

--- ControlTransferStatementsPlayground.playground/Contents.swift
+++ ControlTransferStatementsPlayground.playground/Contents.swift
@@ -61,8 +61,69 @@ import Foundation
     // MARK: 2.2 The continue Keyword
     markHeading3(title: "\(HeadingWordings.section) 2.2")
 
+    private func printOnlyConsonants(ofWord word: String) {
+        let vowels: [Character] = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
+        for letter in word {
+            if vowels.contains(letter) {
+                continue
+            }
+            print(letter, terminator: "")
+        }
+        print("\n")
+    }
+
+    printOnlyConsonants(ofWord: "Code Learn Hub")
+
+    private func countAttemptsOneToTen() {
+        var number = 1                  // current number
+        var attempts = 1                // attempts to reach next number
+        var attemptsPerNumber = [Int]() // saving the attempts of each number, until we reach 10
+        
+        while true {
+            let randomNumber = Int.random(in: -100...100)
+            
+            if number == 10 { // in order to break from the infinite loop
+                break
+            }
+            
+            if randomNumber - number != 1 { // if the number generated isn't the next integer, skip this loop
+                attempts += 1
+                continue
+            }
+            
+            attemptsPerNumber.append(attempts)  // save all attempts
+            attempts = 0                        // reset attempts for next progression/integer
+            number += 1                         // increase the target integer
+        }
+        
+        for (theNumber, theAttempts) in attemptsPerNumber.enumerated() {
+            print("It took us \(theAttempts) attempts to go from \(theNumber + 1) to \(theNumber + 2).")
+        }
+    }
+
+    countAttemptsOneToTen()
+
     // MARK: 2.3 The return Keyword
     markHeading3(title: "\(HeadingWordings.section) 2.3")
+
+private func talkLikeSpongebob(phrase: String) {
+    if phrase.isEmpty {
+        return // exit the function if we have nothing to say
+    }
+    var finalPhrase = ""
+    
+    for (index, letter) in phrase.enumerated() {
+        if index % 2 == 0 {
+            finalPhrase.append(letter.lowercased())
+        } else {
+            finalPhrase.append(letter.uppercased())
+        }
+    }
+    
+    print("\"\(phrase)\" - Someone.\n\"\(finalPhrase)\" - Us")
+}
+
+talkLikeSpongebob(phrase: "I don't like this example.")
 
     // MARK: 2.4 The fallthrough Keyword
     markHeading3(title: "\(HeadingWordings.section) 2.4")
