7e71250baa2348f02fe3d0554440aca4a3f24620
Dimitris Tasios <dtasios4@gmail.com>
2022-07-23 14:15:42 +0300

    remove comments and improve template

--- ArticleTemplatePlayground.playground/Contents.swift
+++ ArticleTemplatePlayground.playground/Contents.swift
@@ -1,8 +1,11 @@
 // MARK: ArticleName
 
+let articleSingleton = Article.shared
 // MARK: - 1. Section1
+articleSingleton.nextHeader(ofLevel: .h1)
 
 // MARK: - 2. Section2
+articleSingleton.nextHeader(ofLevel: .h1)
 
     // MARK:  2.1 Subsection21
 
@@ -11,13 +14,4 @@
             // MARK:  2.1.1.1 Subsection211
 
 // MARK: - 3. Section3
-
-
-
-let articleSingleton = Article.shared
-
-articleSingleton.nextHeader(ofLevel: .h2)
-articleSingleton.nextHeader(ofLevel: .h3)
-articleSingleton.nextHeader(ofLevel: .h1)
-articleSingleton.nextHeader(ofLevel: .h1)
 articleSingleton.nextHeader(ofLevel: .h1)
